979a204435b380619ae7ca9f6eedb261833e3248
Christian Ndu <christiannduh@gmail.com>
2025-05-01 15:39:05 +0100

    feat: add graceful shutdown handler

--- index.ts
+++ index.ts
@@ -23,6 +23,21 @@ app.get("/", (_: Request, res: Response) => {
     res.json({ message: "Hello World!" });
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+// Add graceful shutdown handling
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled rejection:", err);
+    server.close(() => process.exit(1));
+});
+
+// Global error handler
+app.use((err: Error, _: Request, res: Response, next: any) => {
+    console.error(err.stack);
+    res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+    });
 });
